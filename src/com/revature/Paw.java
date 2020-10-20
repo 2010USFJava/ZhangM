75994e2ccd850a59f40c168e4a3b1e01e63bb242
Michelle Zhang <mzhang32@gmail.com>
2020-10-20 13:46:45 -0700

    added paws and cats

---
+++ src/com/revature/Paw.java
@@ -0,0 +1,21 @@
+package com.revature;
+
+public class Paw {
+	private int numToes;
+	private String toeBeanColor;
+	
+	public Paw(int numberOfToes, String toeColor) {
+		this.numToes = numberOfToes;
+		this.toeBeanColor = toeColor;
+	}
+	
+	public int getNumToes() {
+		return numToes;
+	}
+
+	public String getToeBeanColor() {
+		return toeBeanColor;
+	}
+	
+	
+}
